03071b1c22345f87224ca6c9f45d7be4e088ed44
Martin Pandola <WorkForPizza@users.noreply.github.com>
2018-07-13 16:32:27 -0500

    Unnecessary script

--- upgrade.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-curl -sSL -O -L https://github.com/ClearBlade/Edge/releases/download/3.1.1/edge-linux-amd64.tar.gz
-
-tar xzvf edge-linux-amd64.tar.gz
-
-sudo ln -s -f edge-3.1.1 /usr/local/edge
